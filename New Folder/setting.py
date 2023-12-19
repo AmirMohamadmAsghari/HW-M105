cfe5836fe41a08eb061878208933b543d6abf029
Amirmohamad <153849982+AmirMohamadmAsghari@users.noreply.github.com>
2023-12-19 13:11:15 +0330

    Delete New Folder directory

--- New Folder/setting.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST = 'smtp.example.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'amirm2.asghari0@gmail.com'
-EMAIL_HOST_PASSWORD = 'amir'
--- New Folder/urls.py
+++
@@ -1,7 +0,0 @@
-from view import send_email_view
-from django.urls import path
-
-
-urlpatterns = [
-    path('amir13.asghari81@gmail.com', send_email_view, name='send_email')
-]
